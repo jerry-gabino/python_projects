cb461addf54e5895f0aaa2df44c9cfd0e8802487
jerry-gabino <jerry.gabino@gmail.com>
2020-05-10 17:21:12 -0500

    update 5/10/2020 5:20 pm

--- map_function.py
+++ map_function.py
@@ -14,5 +14,16 @@ square = map(func, my_list)
 for i in my_list:
     print(i)
 
+# using lambda function; inline function
+square2 = list(map(lambda elem: elem ** 2, my_list))
+print(square2)
+
 # print the square using the map() function
 print(list(square))
+
+# use map and lambda functions to add corresponding items of two lists
+list_1 = [12, 9, 8]
+list_2 = [10, 5, 4]
+
+output = list(map(lambda n1, n2: n1 + n2, list_1, list_2 ))
+print(output)
